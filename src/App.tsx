c1ea35d2dbc04f999c05562807e6a9afccdba581
Daria Mochalova <shichimiya74@gmail.com>
2026-08-07 13:58:57 +0500

    microtask - 06: useParams and connect puma page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { Error404 } from './components/pages/Error404';
-import { PageOne } from './components/pages/PageOne';
-import { PageThree } from './components/pages/PageThree';
-import { PageTwo } from './components/pages/PageTwo';
+import { PageOne } from './components/pages/Adidas';
+import { PageThree } from './components/pages/Abibas';
+import { PageTwo } from './components/pages/Puma';
 import styles from './components/Site.module.css';
 import { Navigate, NavLink, Route, Routes } from 'react-router';
+import { SneakerModel } from './components/pages/SneakerModel';
+
+const PATH = {
+    PAGE1: '/adidas',
+    PAGE2: '/puma',
+    PAGE3: '/abibas',
+    ERROR: '/error404',
+} as const;
+
+function getLinkClassName(isActive: boolean) {
+    return isActive ? styles.activeNavLink : styles.navLink;
+}
 
 export const App = () => {
     return (
@@ -15,52 +27,53 @@ export const App = () => {
                 <div className={styles.nav}>
                     <div>
                         <NavLink
-                            to={'/page1'}
+                            to={PATH.PAGE1}
                             className={({ isActive }) =>
-                                isActive
-                                    ? `${styles.activeNavLink}`
-                                    : `${styles.navLink}`
+                                getLinkClassName(isActive)
                             }
                         >
-                            Page 1
+                            Adidas
                         </NavLink>
                     </div>
                     <div>
                         <NavLink
-                            to={'/page2'}
+                            to={PATH.PAGE2}
                             className={({ isActive }) =>
-                                isActive
-                                    ? `${styles.activeNavLink}`
-                                    : `${styles.navLink}`
+                                getLinkClassName(isActive)
                             }
                         >
-                            Page 2
+                            Puma
                         </NavLink>
                     </div>
                     <div>
                         <NavLink
-                            to={'/page3'}
+                            to={PATH.PAGE3}
                             className={({ isActive }) =>
-                                isActive
-                                    ? `${styles.activeNavLink}`
-                                    : `${styles.navLink}`
+                                getLinkClassName(isActive)
                             }
                         >
-                            Page 3
+                            Abibas
                         </NavLink>
                     </div>
                 </div>
                 <div className={styles.content}>
                     <Routes>
-                        <Route path='/' element={<Navigate to={'/page1'} />} />
-                        <Route path='/page1' element={<PageOne />} />
-                        <Route path='/page2' element={<PageTwo />} />
-                        <Route path='/page3' element={<PageThree />} />
-                        <Route path='/error404' element={<Error404 />} />
                         <Route
-                            path='/*'
-                            element={<Navigate to={'/error404'} />}
+                            path='/'
+                            element={<Navigate to={PATH.PAGE1} />}
                         />
+                        <Route path={PATH.PAGE1} element={<PageOne />} />
+                        <Route path={PATH.PAGE2} element={<PageTwo />} />
+                        <Route path={PATH.PAGE3} element={<PageThree />} />
+                        <Route
+                            path={'/:model/:id'}
+                            element={<SneakerModel />}
+                        />
+                        <Route
+                            path={'/:model/:id'}
+                            element={<SneakerModel />}
+                        />
+                        <Route path='/*' element={<Error404 />} />
                     </Routes>
                 </div>
             </div>
---
+++ src/components/pages/SneakerModel.tsx
@@ -0,0 +1,98 @@
+import { useParams } from 'react-router';
+import styles from '../Site.module.css';
+import { v1 } from 'uuid';
+import pumaPalermo from '../../assets/puma_1.webp';
+import pumaSpeed from '../../assets/puma_speed.webp';
+import pumaSued from '../../assets/puma_sued.webp';
+import adidasModel1 from '../../assets/AdiFOM_TRXN_Shoes_Black_IG7453_01_standard.webp';
+import adidasModel2 from '../../assets/PostMove_Mid_Cloudfoam_Super_Lifestyle_Basketball_Mid_Classic_Shoes_Black_GY7163_01_standard.webp';
+import adidasModel3 from '../../assets/Superstar_XLG_Shoes_Black_IG9777_01_standard.webp';
+type Model = {
+    id: string;
+    model: string;
+    collection: string;
+    price: string;
+    picture: string;
+};
+type ModelState = Record<string, Array<Model>>;
+export const allModels: ModelState = {
+    adidas: [
+        {
+            id: v1(),
+            model: 'ADIDAS ADIFOM TRXN',
+            collection: 'new collection1',
+            price: '100200$',
+            picture: adidasModel1,
+        },
+        {
+            id: v1(),
+            model: 'ADIDAS ADIFOM SUPER',
+            collection: 'new collection22',
+            price: '200300$',
+            picture: adidasModel2,
+        },
+        {
+            id: v1(),
+            model: 'ADIDAS SUPER SUPERSKI',
+            collection: 'new collection333',
+            price: '300400$',
+            picture: adidasModel3,
+        },
+    ],
+    puma: [
+        {
+            id: v1(),
+            model: 'PUMA PALERMO',
+            collection: 'new collection1',
+            price: '100200$',
+            picture: pumaPalermo,
+        },
+        {
+            id: v1(),
+            model: 'PUMA SPEEDYCAT',
+            collection: 'new collection22',
+            price: '200300$',
+            picture: pumaSpeed,
+        },
+        {
+            id: v1(),
+            model: 'PUMA SUSU',
+            collection: 'new collection333',
+            price: '300400$',
+            picture: pumaSued,
+        },
+    ],
+};
+
+export function SneakerModel() {
+    const params = useParams();
+    console.log(params.model);
+
+    if (!params.model) {
+        return;
+    }
+
+    const currentItem = allModels[params.model].find(
+        (item) => item.id === params.id,
+    );
+    return (
+        <div>
+            {currentItem ? (
+                <>
+                    <h2>{currentItem.model}</h2>
+                    <h3>{currentItem.collection}</h3>
+                    <div>
+                        <img
+                            className={styles.image}
+                            src={currentItem.picture}
+                            alt={currentItem.model}
+                        />
+                    </div>
+                    <span>{currentItem?.price}</span>
+                </>
+            ) : (
+                'Not available'
+            )}
+        </div>
+    );
+}
